e4e44f230ebc058f223676eb4d600279c1880a50
Henry Liu <henryl1993@hotmail.com>
2016-05-01 19:12:31 -0400

    Added distance from friend

--- AndroidApp/app/src/main/java/com/safeshell/safeshell/fragments/FriendsFragment.java
+++ AndroidApp/app/src/main/java/com/safeshell/safeshell/fragments/FriendsFragment.java
@@ -82,10 +82,30 @@ public class FriendsFragment extends ListFragment {
         } 
 
         // Method to get distance from Friend's current location
-        public int getDistance(){
-            // Need to work on 
-            return 0; 
-        }
+        // From http://stackoverflow.com/questions/14394366/find-distance-between-two-points-on-map-using-google-map-api-v2
+        public double CalculationByDistance(LatLng Dest) {
+        // Radius of earth in Km  
+            int Radius=6371;       
+            double lat1 = Dest.latitude;
+            double lat2 = this.location.latitude;
+            double lon1 = Dest.longitude;
+            double lon2 = this.location.longitude;
+            double dLat = Math.toRadians(lat2-lat1);
+            double dLon = Math.toRadians(lon2-lon1);
+            double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+            Math.sin(dLon/2) * Math.sin(dLon/2);
+            double c = 2 * Math.asin(Math.sqrt(a));
+            double valueResult= Radius*c;
+            double km=valueResult/1;
+            DecimalFormat newFormat = new DecimalFormat("####");
+            int kmInDec =  Integer.valueOf(newFormat.format(km));
+            double meter=valueResult%1000;
+            int  meterInDec= Integer.valueOf(newFormat.format(meter));
+            Log.i("Radius Value",""+valueResult+"   KM  "+kmInDec+" Meter   "+meterInDec);
+
+            return Radius * c;
+     }
 
         @Override
         public String toString() {
